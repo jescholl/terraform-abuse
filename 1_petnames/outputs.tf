e6afd76fe5f27256737ca02defddfd011c940dc3
Jason Scholl <jason.e.scholl@gmail.com>
2022-11-19 14:50:25 -0800

    Add first challenge

---
+++ 1_petnames/outputs.tf
@@ -0,0 +1,29 @@
+output "_1_petnames_read" {
+  description = "Number of petnames read"
+  value = null
+}
+
+output "_2_unique_initials" {
+  description = "Number of unique initials found in file"
+  value = null
+}
+
+output "_3_unseen_initials" {
+  description = "Number possible initials that were not found in the file"
+  value = null
+}
+
+output "_4_top_5_most_common" {
+  description = "The top 5 most common initials"
+  value = null
+}
+
+output "_5_histogram" {
+  description = "A Histogram showing the distribution of repeatd initials by number of repeats."
+  value = null
+}
+
+output "_6_top_5_most_common" {
+  description = "ASCII representation of _5_histogram"
+  value = null
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Test your skill with terraform, see how badly you can abuse it
+
+This repo contains a list of challenges testing how badly you can abuse terraform and make it do things it should never do.
+
+## Challenges
+1. [petnames](1_petnames): Read and analyze a file with 1 million 3-word petnames.
